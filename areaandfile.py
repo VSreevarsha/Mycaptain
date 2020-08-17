b878eb0530ae80d7092d2dcc803e5ea3d5f652fe
VSreevarsha <66515385+VSreevarsha@users.noreply.github.com>
2020-08-17 18:59:43 +0530

    radius input and file name input
    area of circle will be displayed
    and file extension will be displayed.

---
+++ areaandfile.py
@@ -0,0 +1,7 @@
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+area = PI * radius * radius
+print("The area of circle with radius is 1.1:", area)
+fn= input("Enter Filename: ")
+f = fn.split(".")
+print ("Extension of the file is : " + f[-1])
