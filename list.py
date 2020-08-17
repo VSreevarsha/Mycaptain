b27441511c3845056a4c248a0e72d6813175dc1d
VSreevarsha <66515385+VSreevarsha@users.noreply.github.com>
2020-08-17 19:48:26 +0530

    created two lists
    list1 and list2

---
+++ list.py
@@ -0,0 +1,22 @@
+# Python program to print positive Numbers in a List 
+  
+# list of numbers 
+list1 = [12, -7, 5, 64, -14] 
+  
+# iterating each number in list 
+for num in list1: 
+      
+    # checking condition 
+    if num >= 0: 
+       print(num, end = " ")
+
+
+       
+list2 = [12,14,-95,3] 
+  
+# iterating each number in list 
+for num1 in list2: 
+      
+    # checking condition 
+    if num1>= 0: 
+       print(num1, end = " ")
