20e21a4ecf12b26a0e670401693bc6b6f3fe4829
VSreevarsha <66515385+VSreevarsha@users.noreply.github.com>
2020-08-17 19:34:36 +0530

    fibonacci series
    n is number o elements to enter

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the terms"))
+a=0                                         
+b=1                                      
+if n<=0
+    print("The requested series is",a)
+else:
+    print(a,b,end=" ")
+    for x in range(2,n):
+        next=a+b                           
+        print(next,end=" ")
+        a=b
+        b=next
